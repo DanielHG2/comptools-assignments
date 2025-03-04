ed012716154a7fd46b751d724e6042f82633ce7e
DanielHG2 <alfonsi.1920888@studenti.uniroma1.it>
2025-03-04 21:30:25 +0100

    modified:   assignment1_python.py

--- assignment1_python.py
+++ assignment1_python.py
@@ -3,14 +3,14 @@ from numpy.linalg import solve
 import numpy as np
 
 # Load the dataset
-df = pd.read_csv('~/Downloads/current.csv')
+df = pd.read_csv('https://www.stlouisfed.org/-/media/project/frbstl/stlouisfed/research/fred-md/monthly/current.csv?sc_lang=en&hash=80445D12401C59CF716410F3F7863B64')
+
 
 # Clean the DataFrame by removing the row with transformation codes
 df_cleaned = df.drop(index=0)
 df_cleaned.reset_index(drop=True, inplace=True)
-
+df_cleaned['sasdate'] = pd.to_datetime(df_cleaned['sasdate'], format= '%m/%d/%Y')
 ## df_cleaned contains the data cleaned
-df_cleaned
 
 # Extract transformation codes
 transformation_codes = df.iloc[0, 1:].to_frame().reset_index()
@@ -57,6 +57,9 @@ def apply_transformation(series, code):
 for series_name, code in transformation_codes.values:
     df_cleaned[series_name] = apply_transformation(df_cleaned[series_name].astype(float), float(code))
 
+
+df_cleaned=df_cleaned[2:]
+df_cleaned.reset_index(drop=True, inplace=True)
 df_cleaned.head()
 
 ############################################################################################################
@@ -145,3 +148,129 @@ beta_ols = solve(X.T @ X, X.T @ y)
 ## Produce the One step ahead forecast
 ## % change month-to-month of INDPRO
 forecast = X_T@beta_ols*100
+
+print(forecast)
+
+###############################################
+## NOW FORECAST ASSIGNMENT
+def calculate_forecast(df_cleaned, p = 4, H = [1,4,8], end_date = '12/1/1999',target = 'INDPRO', xvars = ['CPIAUCSL', 'TB3MS']):
+
+    rt_df = df_cleaned[df_cleaned['sasdate'] <= pd.Timestamp(end_date)]
+    
+    Y_actual = []
+    for h in H:
+        os = pd.Timestamp(end_date) + pd.DateOffset(months=h)
+        Y_actual.append(df_cleaned[df_cleaned['sasdate'] == os][target]*100)
+        
+    Yraw = rt_df[target]
+    Xraw = rt_df[xvars]
+    X = pd.DataFrame()
+    
+    for lag in range(0,p):
+        X[f'{target}_lag{lag}'] = Yraw.shift(lag)
+
+    for col in Xraw.columns:
+        for lag in range(0,p):
+            X[f'{col}_lag{lag}'] = Xraw[col].shift(lag)
+    
+    X.insert(0, 'Ones', np.ones(len(X)))
+    X_T = X.iloc[-1:].values
+
+    Yhat = []
+    for h in H:
+        y_h = Yraw.shift(-h) 
+        y = y_h.iloc[p:-h].values
+        X_ = X.iloc[p:-h].values
+        beta_ols = solve(X_.T @ X_, X_.T @ y)
+        Yhat.append(X_T@beta_ols*100)
+
+    return np.array(Y_actual) - np.array(Yhat)
+
+t0 = pd.Timestamp('12/1/1999')
+e = []
+T = []
+for j in range(0, 10):
+    t0 = t0 + pd.DateOffset(months=1)
+    print(f'Using data up to {t0}')
+    ehat = calculate_forecast(df_cleaned, p = 4, H = [1,4,8], end_date = t0)
+    e.append(ehat.flatten())
+    T.append(t0)
+
+
+edf = pd.DataFrame(e)
+
+RMSFE1 = np.sqrt(edf.apply(np.square).mean())
+
+print(RMSFE1)  ##first model, now different variables
+
+Yraw2 = df_cleaned['INDPRO']
+Xraw2 = df_cleaned[['S&P 500', 'INVEST']]
+
+X2 = pd.DataFrame()
+col = 'INDPRO'
+for lag in range(0,num_lags+1):
+        X2[f'{col}_lag{lag}'] = Yraw2.shift(lag)
+for col in Xraw2.columns:
+    for lag in range(0,num_lags+1):
+        X2[f'{col}_lag{lag}'] = Xraw2[col].shift(lag)
+
+X2.insert(0, 'Ones', np.ones(len(X2)))
+X2_T = X2.iloc[-1:].values
+X2 = X2.iloc[num_lags:-num_leads].values
+forecast2 = X2_T@beta_ols*100
+
+def calculate_forecast2(df_cleaned, p = 4, H = [1,4,8], end_date = '12/1/1999', target2 = 'INDPRO', xvars2 = ['S&P 500', 'INVEST']):
+    rt_df2 = df_cleaned[df_cleaned['sasdate'] <= pd.Timestamp(end_date)]
+    
+    Y2_actual = []
+    for h in H:
+        os = pd.Timestamp(end_date) + pd.DateOffset(months=h)
+        Y2_actual.append(df_cleaned[df_cleaned['sasdate'] == os][target2]*100)
+      ##if os in df_cleaned['sasdate'].values:
+      ##      Y2_actual.append(df_cleaned[df_cleaned['sasdate'] == os][target2].values[0] * 100)
+       ## else:
+       ##     Y2_actual.append(np.nan)  
+
+
+    Yraw2 = rt_df2[target2]
+    Xraw2 = rt_df2[xvars2]
+    X2 = pd.DataFrame()
+    
+    for lag in range(0, p):
+        X2[f'{target2}_lag{lag}'] = Yraw2.shift(lag)
+
+    for col in Xraw2.columns:
+        for lag in range(0, p):
+            X2[f'{col}_lag{lag}'] = Xraw2[col].shift(lag)
+    
+    X2.insert(0, 'Ones', np.ones(len(X2)))
+    X2_T = X2.iloc[-1:].values
+
+    Yhat2 = []
+    for h in H:
+        y2_h = Yraw2.shift(-h) 
+        y2 = y2_h.iloc[p:-h].values
+        X2_ = X2.iloc[p:-h].values
+        ##if X2_.shape[0] != y2.shape[0]:
+        ##raise ValueError(f"Dimension mismatch: X2_ has {X2_.shape[0]} rows, but y2 has {y2.shape[0]} rows")
+        beta_ols2 = solve(X2_.T @ X2_, X2_.T @ y2)
+        Yhat2.append(X2_T @ beta_ols2 * 100)
+
+    return np.array(Y2_actual) - np.array(Yhat2)
+
+t0 = pd.Timestamp('12/1/1999')
+e2 = []
+T2 = []
+for j in range(0, 10):
+    t0 = t0 + pd.DateOffset(months=1)
+    print(f'Using data up to {t0}')
+    ehat2 = calculate_forecast2(df_cleaned, p = 4, H = [1,4,8], end_date = t0)
+    e2.append(ehat2.flatten())
+    T2.append(t0)
+
+edf2 = pd.DataFrame(e2)
+
+RMSFE2 = np.sqrt(edf2.apply(np.square).mean())
+print(RMSFE2)
+
+### with such 2 covariates, forecast is worse comparing RMSFE !!
